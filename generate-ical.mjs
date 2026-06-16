f5dc3fb31d008cd5be1bc0635a835e3a5a1ff457
djebsoft <djebbour1985@gmail.com>
2026-06-16 18:51:30 +0100

    feat: skeleton structure

--- generate-ical.mjs
+++ generate-ical.mjs
@@ -1,7 +1,11 @@
-// This is a placeholder file which shows how you can access functions and data defined in other files. You can delete the contents of the file once you have understood how it works.
-// It can be run with `node`.
-
-import { getGreeting } from "./common.mjs";
+import { getFloatingDay } from "./common.mjs";
 import daysData from "./days.json" with { type: "json" };
+const start_year = 2020;
+const end_year = 2030;
+for (let day of daysData) {
+    for (let year = start_year; year <= end_year; year++) {
+        const dayOfMonth = getFloatingDay(year, day.monthName, day.dayName, day.occurrence);
+        console.log(`${day.name}: ${year}-${day.monthName}-${dayOfMonth}`);
+    }
+}
 
-console.log(`{getGreeting()} - there are ${daysData.length} known days`);
